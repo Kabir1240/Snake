00263fada1562b475c9e14e7943970356010c346
Kabir Kashif <kashif.kabir60@gmail.com>
2024-04-10 02:35:56 +0500

    converted program to an exe file

--- main.py
+++ main.py
@@ -7,29 +7,7 @@ import time
 
 """
 Main logic for the snake game using Turtle Graphics.
-:return: None
 """
-
-# initializes the snake, food and scoreboard
-snake = Snake()
-food = Food()
-score = Scoreboard()
-
-# initializes the screen
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake")
-screen.tracer(0)
-
-# listens for user inputs and makes the snake act accordingly
-screen.listen()
-screen.onkey(key="Up", fun=snake.up)
-screen.onkey(key="Down", fun=snake.down)
-screen.onkey(key="Left", fun=snake.left)
-screen.onkey(key="Right", fun=snake.right)
-
-
 def user_prompt() -> bool:
     """
     Allows user to decide if they want to continue the game or not using a prompt display via turtle graphics
@@ -48,45 +26,65 @@ def user_prompt() -> bool:
         return True
 
 
-# main game loop
-collision_occurred = False
-user_continue_game = False
-while not collision_occurred:
-    screen.update()
-    time.sleep(0.08)
-    snake.move()
+if __name__ == "__main__":
+    # initializes the snake, food and scoreboard
+    snake = Snake()
+    food = Food()
+    score = Scoreboard()
 
-    # detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        food.random_color()
-        score.increase_score()
-        snake.extend()
+    # initializes the screen
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake")
+    screen.tracer(0)
 
-    # detect collision with wall
-    if snake.head.xcor() >= 295 or snake.head.xcor() <= -295 or snake.head.ycor() >= 295 or snake.head.ycor() <= -295:
-        score.game_over()
-        collision_occurred = True
-        user_continue_game = not user_prompt()
+    # listens for user inputs and makes the snake act accordingly
+    screen.listen()
+    screen.onkey(key="Up", fun=snake.up)
+    screen.onkey(key="Down", fun=snake.down)
+    screen.onkey(key="Left", fun=snake.left)
+    screen.onkey(key="Right", fun=snake.right)
 
-    # detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 3:
+    # main game loop
+    collision_occurred = False
+    user_continue_game = False
+    while not collision_occurred:
+        screen.update()
+        time.sleep(0.08)
+        snake.move()
+
+        # detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            food.random_color()
+            score.increase_score()
+            snake.extend()
+
+        # detect collision with wall
+        if snake.head.xcor() >= 295 or snake.head.xcor() <= -295 or snake.head.ycor() >= 295 or snake.head.ycor() <= -295:
             score.game_over()
             collision_occurred = True
             user_continue_game = not user_prompt()
 
-    # if a collision has occurred, the game will either restart or end depending on the user input
-    if collision_occurred:
-        if user_continue_game:
-            snake.reset()
-            score.reset_score()
-            screen.listen()
-            collision_occurred = False
-        if not user_continue_game:
-            snake.remove_snake()
-            screen.update()
+        # detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 3:
+                score.game_over()
+                collision_occurred = True
+                user_continue_game = not user_prompt()
+
+        # if a collision has occurred, the game will either restart or end depending on the user input
+        if collision_occurred:
+            if user_continue_game:
+                snake.reset()
+                score.reset_score()
+                screen.listen()
+                collision_occurred = False
+            if not user_continue_game:
+                snake.remove_snake()
+                screen.update()
 
 
-# exit
-screen.exitonclick()
+    # exit
+    screen.exitonclick()
